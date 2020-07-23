78d032ca88a2ee4d987613fa4873d4cf4cbd6a4c
algoribi <algoribi36@gmail.com>
2020-07-23 13:23:04 +0900

    make Post Detail

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class PostList(ListView) :
     model = Post
 
     def get_queryset(self):
         return Post.objects.order_by('-created')
+
+class PostDetail(DetailView) :
+    model = Post
+
+# def post_detail(request, pk) :
+#     blog_post = Post.objects.get(pk=pk)
+#
+#     return rander(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'blog_post':blog_post,
+#         }
+#     )
 
 # def index(request) :
 #     Posts = Post.objects.all()
